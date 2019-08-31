e4d8026fdaeb54d1e7ce6fdb164232efc8ed3a1c
Pro100mishka <pro100gnomik@gmail.com>
2019-08-31 18:02:24 +0600

    HW2
    HW2

--- src/main/java/com/geekbrains/controllers/MainController.java
+++ src/main/java/com/geekbrains/controllers/MainController.java
@@ -1,6 +1,5 @@
 package com.geekbrains.controllers;
 
-import com.geekbrains.entities.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +10,5 @@ public class MainController {
     @RequestMapping("/")
     public String showHomePage() {
         return "index";
-    }
-
-    @GetMapping("/info")
-    public String showInfoPage(Model model) {
-        String msg = "Java";
-        Student bob = new Student();
-        bob.setFirstName("Bob");
-
-        model.addAttribute("message123", msg);
-        model.addAttribute("student", bob);
-
-        return "info";
     }
 }
